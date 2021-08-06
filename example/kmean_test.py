740d4235157cbced4f4da2a3f99d0aa67f6f4321
ZeroSum0x00 <nguyenichthanhtu0509@gmail.com>
2021-08-06 10:34:22 +0700

    add Kmean Clustering

--- example/kmean_test.py
+++ example/kmean_test.py
@@ -1,6 +1,6 @@
-import numpy as np
 from utils.preprocess import Preprocessing
 from utils.visualizer import Visualizer
+from unsupervised_learning.kmean import KMeans_Clustering
 
 if __name__ == '__main__':
     data = '../datasets/the_trang.csv'
@@ -9,9 +9,17 @@ if __name__ == '__main__':
     dict_y = ['Loai']
     pp = Preprocessing()
     pp.get_label(label)
-    X_train, y_train = pp.get_data(data, dict_X=dict_X, dict_y=dict_y)
-    X_test = np.array([[170, 52]])
+    X, y = pp.get_data(data, dict_X=dict_X, dict_y=dict_y)
+
+    k = 6
+    model = KMeans_Clustering(n_clusters=k)
+    center_points = model.predict(X=X)
 
     visual = Visualizer()
-    visual.plot_data_2D(X_train[:, 0], X_train[:, 1])
+    for i in range(k):
+        X_i = X[model.label_pred == i]
+        visual.plot_data_2D(X_i[:, 0], X_i[:, 1], label=pp.label_mean[i])
+
+    visual.plot_point_2D(center_points, shape='Xk', label='Center Points')
+    visual.plot_saved('../saved_weights/kmean.png')
     visual.plot_show('Visualization K-Means Clustering model', dict_X[0], dict_X[1])
--- unsupervised_learning/kmean.py
+++ unsupervised_learning/kmean.py
@@ -2,11 +2,30 @@ import numpy as np
 from scipy.spatial.distance import cdist
 
 class KMeans_Clustering:
-    def __init__(self, k):
-        self.k = k
-
-    def fit(self, X, y):
-        pass
+    def __init__(self, n_clusters=3, max_iters=1500):
+        self.n_clusters = n_clusters
+        self.max_iters = max_iters
+        self.center_points = None
+        self.label_pred = None
 
     def predict(self, X):
-        pass
+        # Tạo self.n_clusters điểm bất kỳ, các điểm này thuộc các điểm có trên dữ liệu
+        self.center_points = X[np.random.choice(X.shape[0], self.n_clusters, replace=False)]
+        for iter in range(self.max_iters):
+            # Tính khoảng cách từ các điểm có trên dữ liệu đến self.n_clusters điểm mới tạo
+            distances = cdist(X, self.center_points)
+            # Tìm ra tập label (là tập chứa vị trí điểm có khoảng cách đến tâm nhỏ nhất)
+            self.label_pred = np.argmin(distances, axis=1)
+
+            center_compare = self.center_points
+
+            self.center_points = []
+            for i in range(self.n_clusters):
+                self.center_points.append(np.mean(X[self.label_pred == i], axis=0))
+
+            self.center_points = np.array(self.center_points)
+
+            if set(tuple(c) for c in self.center_points) == set(tuple(c) for c in center_compare):
+                break
+
+        return self.center_points
