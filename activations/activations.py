2ba3b6570cee35479c1cab33089a303e7dadf69e
ZeroSum0x00 <nguyenichthanhtu0509@gmail.com>
2021-10-14 04:40:16 +0700

    fit activations1

--- activations/activations.py
+++ activations/activations.py
@@ -6,5 +6,14 @@ def linear(x):
 def sigmoid(x):
     return 1.0 / (1 + np.exp(-x))
 
+def tanh(x):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
 def relu(x):
-    return np.where(x >= 0, 1, 0)
+    return np.where(x >= 0, x, 0)
+
+def leaky_relu(x):
+    return np.where(x >= 0, x, 0.01 * x)
+
+def swish(x):
+    return x / (1 + np.exp(-x))
