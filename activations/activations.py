1c4a217b46275166c8c8dd502870637b78c5554f
ZeroSum0x00 <nguyenichthanhtu0509@gmail.com>
2021-10-14 11:45:33 +0700

    add activation class

--- activations/activations.py
+++ activations/activations.py
@@ -1,19 +1,65 @@
 import numpy as np
 
-def linear(x):
-    return x
 
-def sigmoid(x):
-    return 1.0 / (1 + np.exp(-x))
+class _activation(object):
 
-def tanh(x):
-    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+    def forward(self):
+        raise NotImplemented
 
-def relu(x):
-    return np.where(x >= 0, x, 0)
+    def derivative(self):
+        raise NotImplemented
 
-def leaky_relu(x):
-    return np.where(x >= 0, x, 0.01 * x)
 
-def swish(x):
-    return x / (1 + np.exp(-x))
+class linear(_activation):
+    def __init__(self, x):
+        self.x = x
+
+    def forward(self):
+        return self.x
+
+    def derivative(self):
+        return 1
+
+
+class sigmoid(_activation):
+    def __init__(self, x):
+        self.x = x
+
+    def forward(self):
+        return 1.0 / (1 + np.exp(-self.x))
+
+    def derivative(self):
+        return self.forward() * (1 - self.forward())
+
+
+class tanh(_activation):
+    def __init__(self, x):
+        self.x = x
+
+    def forward(self):
+        return (np.exp(self.x) - np.exp(-self.x)) / (np.exp(self.x) + np.exp(-self.x))
+
+    def derivative(self):
+        return 1 - self.forward() ** 2
+
+
+class relu(_activation):
+    def __init__(self, x):
+        self.x = x
+
+    def forward(self):
+        return np.where(self.x >= 0, self.x, 0)
+
+    def derivative(self):
+        return np.where(self.x >= 0, 1, 0)
+
+
+class leaky_relu(_activation):
+    def __init__(self, x):
+        self.x = x
+
+    def forward(self):
+        return np.where(self.x >= 0, self.x, 0.01 * self.x)
+
+    def derivative(self):
+        return np.where(self.x >= 0, 1, 0.01)
